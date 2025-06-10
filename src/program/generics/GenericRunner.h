26b8c685dd2451c713ceafd4ee43993d74e850fc
boooiil <boooiil@outlook.com>
2025-06-10 11:33:09 -0400

    update prepare

--- src/program/generics/GenericRunner.h
+++ src/program/generics/GenericRunner.h
@@ -1,11 +1,13 @@
 #ifndef GENERIC_RUNNER_H
 #define GENERIC_RUNNER_H
 
+#include <string>
 #include <type_traits>
+#include <vector>
 
 class GenericRunner {
- public:
-  virtual void prepare(void) = 0;
+public:
+  virtual void prepare(std::vector<std::string>& args) = 0;
   virtual void run(void) = 0;
   virtual void end(void) = 0;
 
@@ -15,7 +17,7 @@ class GenericRunner {
   template <typename T>
   T* getRunner(void) {
     static_assert(std::is_base_of<GenericRunner, T>::value,
-                  "T must be a subclass of GenericRunner");
+      "T must be a subclass of GenericRunner");
     return dynamic_cast<T*>(this);
   };
 };
