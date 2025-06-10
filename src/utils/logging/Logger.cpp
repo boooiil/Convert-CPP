f990b6d2ac0c9b63d67736bc8a597be5520a0c61
boooiil <boooiil@outlook.com>
2025-06-10 11:30:10 -0400

    default debug to true

--- src/utils/logging/Logger.cpp
+++ src/utils/logging/Logger.cpp
@@ -1,5 +1,4 @@
 #include "Logger.h"
 
 // Definition of the static member
-bool Logger::debug_flag =
-    false;  // Initialize it with a default value (e.g., false)
+bool Logger::debug_flag = true;  // Initialize it with a default value (e.g., false)
