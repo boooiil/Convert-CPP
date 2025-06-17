c0b575b1f637ef6dccfe0b06e2bdf999e40c7acd
boooiil <boooiil@outlook.com>
2025-06-17 02:16:06 -0400

    formatting i think

--- src/program/child/media/MediaProcessConversion.cpp
+++ src/program/child/media/MediaProcessConversion.cpp
@@ -4,21 +4,20 @@
 #include <stdexcept>
 #include <string>
 
-#include "../../../utils/logging/Logger.h"
 #include "../../../utils/RegexUtils.h"
 #include "../../../utils/StringUtils.h"
+#include "../../../utils/logging/Logger.h"
 #include "../../Program.h"
+#include "../../settings/Settings.h"
 #include "../../settings/arguments/ArgumentRegistry.h"
 #include "../../settings/arguments/FlagArgument.h"
 #include "../../settings/enums/Activity.h"
 #include "../../settings/enums/Encoders.h"
-#include "../../settings/Settings.h"
 #include "Media.h"
 #include "MediaProcess.h"
 
 MediaProcessConversion::MediaProcessConversion(Media* media)
-  : MediaProcess(media) {
-}
+    : MediaProcess(media) {}
 
 MediaProcessConversion::~MediaProcessConversion() {
   MediaProcess::~MediaProcess();
@@ -27,7 +26,8 @@ MediaProcessConversion::~MediaProcessConversion() {
 void MediaProcessConversion::parse(std::string data) {
   LOG_DEBUG("PARSING LINE:", data);
 
-  ChildOptions& childOptions = *Program::settings->childOptionsMap[this->object->id];
+  ChildOptions& childOptions =
+      *Program::settings->childOptionsMap[this->object->id];
   ArgumentRegistry& argumentRegistry = *childOptions.argumentRegistry;
 
   // Return fail IF:
@@ -35,16 +35,16 @@ void MediaProcessConversion::parse(std::string data) {
   // 3. Encode fails to load nvcuda.dll
   // 4. Encode fails to find an nvidia device
   if (RegexUtils::isMatch(data, "openencodesessionex failed: out of memory",
-    std::regex::icase) ||
-    RegexUtils::isMatch(data, "no capable devices found",
-      std::regex::icase) ||
-    RegexUtils::isMatch(data, "cannot load nvcuda.dll", std::regex::icase) ||
-    RegexUtils::isMatch(data, "device type cuda needed for codec",
-      std::regex::icase)) {
+                          std::regex::icase) ||
+      RegexUtils::isMatch(data, "no capable devices found",
+                          std::regex::icase) ||
+      RegexUtils::isMatch(data, "cannot load nvcuda.dll", std::regex::icase) ||
+      RegexUtils::isMatch(data, "device type cuda needed for codec",
+                          std::regex::icase)) {
     // if the user wants to use hardware encoding (nvenc, amf, qsv)
 
-    FlagArgument& hweFlag = *Program::settings->childOptionsMap[this->object->id]->argumentRegistry->get_t<FlagArgument>(Command::HARDWAREENCODE);
-
+    FlagArgument& hweFlag = *childOptions.argumentRegistry->get_t<FlagArgument>(
+        Command::HARDWAREENCODE);
 
     if (hweFlag) {
       this->object->setActivity(Activity::FAILED_HARDWARE);
@@ -52,48 +52,41 @@ void MediaProcessConversion::parse(std::string data) {
     }
 
     switch (childOptions.runningEncoder) {
-    case Encoders::AV1_AMF:
-    case Encoders::AV1_NVENC:
-    case Encoders::AV1_QSV:
-    case Encoders::H264_AMF:
-    case Encoders::H264_NVENC:
-    case Encoders::H264_QSV:
-    case Encoders::HEVC_AMF:
-    case Encoders::HEVC_NVENC:
-    case Encoders::HEVC_QSV:
-      this->object->setActivity(Activity::FAILED_HARDWARE);
-      break;
-    default:
-      throw std::runtime_error(
-        "Out of memory even though hardware encoding is disabled. This "
-        "should not happen.");
-
+      case Encoders::AV1_AMF:
+      case Encoders::AV1_NVENC:
+      case Encoders::AV1_QSV:
+      case Encoders::H264_AMF:
+      case Encoders::H264_NVENC:
+      case Encoders::H264_QSV:
+      case Encoders::HEVC_AMF:
+      case Encoders::HEVC_NVENC:
+      case Encoders::HEVC_QSV:
+        this->object->setActivity(Activity::FAILED_HARDWARE);
+        break;
+      default:
+        throw std::runtime_error(
+            "Out of memory even though hardware encoding is disabled. This "
+            "should not happen.");
     };
   }
 
   // If the file is already encoded, set the process status to validating
   else if (RegexUtils::isMatch(data, "already exists", std::regex::icase)) {
     this->object->setActivity(Activity::WAITING_VALIDATE);
-  }
-  else if (RegexUtils::isMatch(data, "no such file", std::regex::icase)) {
+  } else if (RegexUtils::isMatch(data, "no such file", std::regex::icase)) {
     this->object->setActivity(Activity::FAILED_FILE_MISSING);
-  }
-  else if (RegexUtils::isMatch(data, "matches no streams",
-    std::regex::icase)) {
+  } else if (RegexUtils::isMatch(data, "matches no streams",
+                                 std::regex::icase)) {
     this->object->setActivity(Activity::FAILED_INVALID_AUDIO_STREAMS);
-  }
-  else if (RegexUtils::isMatch(data, "Invalid duration for option ss")) {
+  } else if (RegexUtils::isMatch(data, "Invalid duration for option ss")) {
     this->object->setActivity(Activity::FAILED_INVALID_DURATION_SS);
-  }
-  else if (RegexUtils::isMatch(data, "Invalid duration for option to")) {
+  } else if (RegexUtils::isMatch(data, "Invalid duration for option to")) {
     this->object->setActivity(Activity::FAILED_INVALID_DURATION_TO);
-  }
-  else if (RegexUtils::isMatch(data, "Unknown encoder")) {
+  } else if (RegexUtils::isMatch(data, "Unknown encoder")) {
     this->object->setActivity(Activity::FAILED_INVALID_ENCODER);
-  }
-  else if (StringUtils::contains(data, "Rematrix is needed between") ||
-    StringUtils::contains(data, "Failed to configure output pad") ||
-    StringUtils::contains(data, "Error reinitializing filters")) {
+  } else if (StringUtils::contains(data, "Rematrix is needed between") ||
+             StringUtils::contains(data, "Failed to configure output pad") ||
+             StringUtils::contains(data, "Error reinitializing filters")) {
     this->object->setActivity(Activity::FAILED_INVALID_AUDIO_CHANNELS);
   }
 
@@ -101,13 +94,13 @@ void MediaProcessConversion::parse(std::string data) {
     LOG_DEBUG("PARSING LINE");
 
     std::string quality =
-      RegexUtils::getFirstMatch(data, "q=(\\d+\\.\\d+|-\\d+\\.\\d+)");
+        RegexUtils::getFirstMatch(data, "q=(\\d+\\.\\d+|-\\d+\\.\\d+)");
     std::string bitrate =
-      RegexUtils::getFirstMatch(data, "bitrate= ?(\\d+\\.\\d+)");
+        RegexUtils::getFirstMatch(data, "bitrate= ?(\\d+\\.\\d+)");
     std::string completedFrames =
-      RegexUtils::getFirstMatch(data, "frame=.+?(\\d+)");
+        RegexUtils::getFirstMatch(data, "frame=.+?(\\d+)");
     std::string fps =
-      RegexUtils::getFirstMatch(data, "fps=.+?(\\d+\\.\\d+|\\d+)");
+        RegexUtils::getFirstMatch(data, "fps=.+?(\\d+\\.\\d+|\\d+)");
     ;
 
     if (quality == "") quality = "-1.0";
