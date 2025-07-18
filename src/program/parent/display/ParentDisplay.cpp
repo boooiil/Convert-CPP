e8bbcb786951ba1743e3de0d4d30241974d5d09a
boooiil <boooiil@outlook.com>
2025-07-18 19:54:48 -0400

    call child info on printInformation, create printInformationTyped func

--- src/program/parent/display/ParentDisplay.cpp
+++ src/program/parent/display/ParentDisplay.cpp
@@ -1,26 +1,68 @@
 #include "ParentDisplay.h"
 
+#include "../../../utils/logging/LogColor.h"
 #include "../../../utils/logging/Logger.h"
 #include "../../Program.h"
-#include "../../generics/GenericRunner.h"
+#include "../../child/display/ChildDisplay.h"
 #include "../../ticker/NTicker.h"
 #include "../Parent.h"
 
-void ParentDisplay::print(void) {
+// NOLINTBEGIN(modernize-return-braced-init-list)
+inline auto tab(int spaces) -> std::string {
+  return std::string(spaces * 2, ' ');
+}
+// NOLINTEND(modernize-return-braced-init-list)
+void ParentDisplay::print() {
   // TODO: finish
 }
 
-void ParentDisplay::printDebug(void) {
+void ParentDisplay::printDebug() {
   // TODO: finish
 }
 
-void ParentDisplay::printInformation(void) {
+void ParentDisplay::printInformationTyped(NTicker* ticker, Parent* parent) {
+  assert(ticker != nullptr);
+  assert(parent != nullptr);
+
   LOG_DEBUG("Printing Information");
 
-  NTicker* ticker = Program::ticker->getRunner<NTicker>();
-  Parent* runner = ticker->runner->getRunner<Parent>();
+  LOG("Parent size:", parent->pending.size());
+
+  LOG(LogColor::fgBlack("Black"), LogColor::fgRed("Red"),
+      LogColor::fgGreen("Green"), LogColor::fgGray("Gray"),
+      LogColor::fgYellow("Yellow"), LogColor::fgBlue("Blue"),
+      LogColor::fgOrange("Orange"), LogColor::fgMagenta("Magenta"),
+      LogColor::fgCyan("Cyan"), LogColor::fgWhite("White"));
+
+  if (parent->pending.empty()) {
+    LOG(LogColor::fgRed("No media files found."));
+    return;
+  }
+
+  /********************************************
+   *  ITERATE TRHOUGH PARENT PENDING CHILDREN  *
+   *********************************************/
+  std::queue<Child*> child_t_queue;
+  while (!parent->pending.empty()) {
+    Child* child = parent->pending.front();
+    parent->pending.pop();
+
+    if (child->pending.empty()) {
+      LOG_DEBUG("Child has no pending media files.");
+      continue;
+    }
+
+    ChildDisplay childDisplay;
+    childDisplay.printInformation(ticker, child);
+
+    // push the child back into the parent's pending queue
+    child_t_queue.push(child);
+  }
+
+  // assign the temp child queue to the parent's pending queue
+  parent->pending = child_t_queue;
 }
 
-void ParentDisplay::printJSON(void) {
+void ParentDisplay::printJSON() {
   // TODO: finish
 }
