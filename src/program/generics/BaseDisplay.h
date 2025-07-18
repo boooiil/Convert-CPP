23e9b16472ac545c77bf244f1512313993f9d009
boooiil <boooiil@outlook.com>
2025-07-18 19:51:11 -0400

    create for base display objs

---
+++ src/program/generics/BaseDisplay.h
@@ -0,0 +1,16 @@
+#ifndef BASE_DISPLAY_H
+#define BASE_DISPLAY_H
+
+#include "JSONSerializableRunner.h"
+
+class BaseDisplay {
+public:
+	virtual ~BaseDisplay(void) = default;
+  virtual void print(void) = 0;
+  virtual void printDebug(void) = 0;
+  virtual void printInformation(JSONSerializableRunner* ticker, JSONSerializableRunner* runner) = 0;
+  virtual void printJSON(void) = 0;
+};
+
+
+#endif // !BASE_DISPLAY_H
