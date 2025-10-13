181cfe25e7977a32c8867226fa0d5d2f5c401ea8
Greg <boooiil@outlook.com>
2025-10-13 13:10:39 -0400

    Refactor VideoCodecFactory to use codec map
    Refactor video codec creation methods to use a map for string-based codec retrieval.

--- src/program/child/ffmpeg/video/VideoCodecFactory.h
+++ src/program/child/ffmpeg/video/VideoCodecFactory.h
@@ -2,6 +2,7 @@
 #define VIDEO_CODEC_FACTORY_H
 
 #include "../../../settings/enums/Encoders.h"
+#include "BaseVideoCodec.h"
 #include "VideoCodec_AV1.h"
 #include "VideoCodec_AV1_AMF.h"
 #include "VideoCodec_AV1_NVENC.h"
@@ -14,10 +15,13 @@
 #include "VideoCodec_HEVC_AMF.h"
 #include "VideoCodec_HEVC_NVENC.h"
 #include "VideoCodec_HEVC_QSV.h"
+#include <functional>
+#include <string>
+#include <unordered_map>
 
 class VideoCodecFactory {
 public:
-  static auto createVideoCodec(Encoders videoCodec) -> BaseVideoCodec * {
+  static auto create(Encoders videoCodec) -> BaseVideoCodec * {
     switch (videoCodec) {
     case Encoders::AV1:
       return new VideoCodec_AV1();
@@ -47,29 +51,71 @@ public:
       return nullptr;
     }
   }
-  static auto createVideoCodec(std::string videoCodec) -> BaseVideoCodec * {
-    if (videoCodec == "av1") {
-      return new VideoCodec_AV1();
-    } else if (videoCodec == "av1_amf") {
-      return new VideoCodec_AV1_AMF();
-    } else if (videoCodec == "av1_nvenc") {
-      return new VideoCodec_AV1_NVENC();
-    } else if (videoCodec == "h264") {
-      return new VideoCodec_H264();
-    } else if (videoCodec == "h264_amf") {
-      return new VideoCodec_H264_AMF();
-    } else if (videoCodec == "h264_nvenc") {
-      return new VideoCodec_H264_NVENC();
-    } else if (videoCodec == "hevc") {
-      return new VideoCodec_HEVC();
-    } else if (videoCodec == "hevc_amf") {
-      return new VideoCodec_HEVC_AMF();
-    } else if (videoCodec == "hevc_nvenc") {
-      return new VideoCodec_HEVC_NVENC();
-    } else {
-      return nullptr;
+  static auto create(std::string videoCodec) -> BaseVideoCodec * {
+
+    if (codec_map.empty()) {
+      initialize();
     }
-  };
+
+    auto it = codec_map.find(videoCodec);
+    if (it != codec_map.end()) {
+      return it->second();
+    }
+
+    return nullptr;
+  }
+
+private:
+  auto static initialize() -> void {
+
+    auto av1 = []() { return new VideoCodec_AV1(); };
+    for (auto &alias : VideoCodec_AV1().getAliases()) {
+      codec_map[alias] = av1;
+    }
+
+    auto av1_amf = []() { return new VideoCodec_AV1_AMF(); };
+    for (auto &alias : VideoCodec_AV1_AMF().getAliases()) {
+      codec_map[alias] = av1_amf;
+    }
+
+    auto av1_nvenc = []() { return new VideoCodec_AV1_NVENC(); };
+    for (auto &alias : VideoCodec_AV1_NVENC().getAliases()) {
+      codec_map[alias] = av1_nvenc;
+    }
+
+    auto h264 = []() { return new VideoCodec_H264(); };
+    for (auto &alias : VideoCodec_H264().getAliases()) {
+      codec_map[alias] = h264;
+    }
+
+    auto h264_amf = []() { return new VideoCodec_H264_AMF(); };
+    for (auto &alias : VideoCodec_H264_AMF().getAliases()) {
+      codec_map[alias] = h264_amf;
+    }
+
+    auto h264_nvenc = []() { return new VideoCodec_H264_NVENC(); };
+    for (auto &alias : VideoCodec_H264_NVENC().getAliases()) {
+      codec_map[alias] = h264_nvenc;
+    }
+
+    auto hevc = []() { return new VideoCodec_HEVC(); };
+    for (auto &alias : VideoCodec_HEVC().getAliases()) {
+      codec_map[alias] = hevc;
+    }
+
+    auto hevc_amf = []() { return new VideoCodec_HEVC_AMF(); };
+    for (auto &alias : VideoCodec_HEVC_AMF().getAliases()) {
+      codec_map[alias] = hevc_amf;
+    }
+
+    auto hevc_nvenc = []() { return new VideoCodec_HEVC_NVENC(); };
+    for (auto &alias : VideoCodec_HEVC_NVENC().getAliases()) {
+      codec_map[alias] = hevc_nvenc;
+    }
+  }
+
+  static std::unordered_map<std::string, std::function<BaseVideoCodec *()>>
+      codec_map;
 };
 
 #endif // VIDEO_CODEC_FACTORY_H
