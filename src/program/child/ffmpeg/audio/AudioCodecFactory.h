62d77577370a9995dc45b95bdb8ce264dbcd647d
boooiil <boooiil@outlook.com>
2025-10-24 04:02:16 -0400

    refactor factories: decouple from concrete codec headers and enum-based creation

--- src/program/child/ffmpeg/audio/AudioCodecFactory.h
+++ src/program/child/ffmpeg/audio/AudioCodecFactory.h
@@ -1,15 +1,6 @@
 #ifndef AUDIO_CODEC_FACTORY_H
 #define AUDIO_CODEC_FACTORY_H
 
-#include "../../../settings/enums/AudioCodec.h"
-#include "AudioCodec_AAC.h"
-#include "AudioCodec_AC3.h"
-#include "AudioCodec_EAC3.h"
-#include "AudioCodec_FLAC.h"
-#include "AudioCodec_MP3.h"
-#include "AudioCodec_OPUS.h"
-#include "AudioCodec_TRUEHD.h"
-#include "AudioCodec_VORBIS.h"
 #include "BaseAudioCodec.h"
 #include <functional>
 #include <string>
@@ -17,94 +8,10 @@
 
 class AudioCodecFactory {
 public:
-  static auto create(AudioCodec audioCodec) -> BaseAudioCodec * {
-    switch (audioCodec) {
-    case AudioCodec::AAC:
-      return new AudioCodec_AAC();
-    case AudioCodec::AC3:
-      return new AudioCodec_AC3();
-    case AudioCodec::EAC3:
-      return new AudioCodec_EAC3();
-    case AudioCodec::FLAC:
-      return new AudioCodec_FLAC();
-    case AudioCodec::MP3:
-      return new AudioCodec_MP3();
-    case AudioCodec::OPUS:
-      return new AudioCodec_OPUS();
-    case AudioCodec::TRUEHD:
-      return new AudioCodec_TRUEHD();
-    case AudioCodec::VORBIS:
-      return new AudioCodec_VORBIS();
-    case AudioCodec::INVALID:
-      [[fallthrough]];
-    case AudioCodec::NONE:
-      return nullptr;
-    }
-  }
-  static auto create(const std::string &audioCodecName) -> BaseAudioCodec * {
-    if (audioCodecName == "aac") {
-      return new AudioCodec_AAC();
-    } else if (audioCodecName == "ac3") {
-      return new AudioCodec_AC3();
-    } else if (audioCodecName == "eac3") {
-      return new AudioCodec_EAC3();
-    } else if (audioCodecName == "flac") {
-      return new AudioCodec_FLAC();
-    } else if (audioCodecName == "mp3") {
-      return new AudioCodec_MP3();
-    } else if (audioCodecName == "opus") {
-      return new AudioCodec_OPUS();
-    } else if (audioCodecName == "truehd") {
-      return new AudioCodec_TRUEHD();
-    } else if (audioCodecName == "vorbis") {
-      return new AudioCodec_VORBIS();
-    }
-    return nullptr;
-  }
+  static auto create(const std::string &name) -> BaseAudioCodec *;
 
 private:
-  static auto initialize() -> void {
-    auto aac = []() -> BaseAudioCodec * { return new AudioCodec_AAC(); };
-    for (auto &alias : AudioCodec_AAC().getAliases()) {
-      codec_map[alias] = aac;
-    }
-
-    auto ac3 = []() -> BaseAudioCodec * { return new AudioCodec_AC3(); };
-    for (auto &alias : AudioCodec_AC3().getAliases()) {
-      codec_map[alias] = ac3;
-    }
-
-    auto eac3 = []() -> BaseAudioCodec * { return new AudioCodec_EAC3(); };
-    for (auto &alias : AudioCodec_EAC3().getAliases()) {
-      codec_map[alias] = eac3;
-    }
-
-    auto flac = []() -> BaseAudioCodec * { return new AudioCodec_FLAC(); };
-    for (auto &alias : AudioCodec_FLAC().getAliases()) {
-      codec_map[alias] = flac;
-    }
-
-    auto mp3 = []() -> BaseAudioCodec * { return new AudioCodec_MP3(); };
-    for (auto &alias : AudioCodec_MP3().getAliases()) {
-      codec_map[alias] = mp3;
-    }
-
-    auto opus = []() -> BaseAudioCodec * { return new AudioCodec_OPUS(); };
-    for (auto &alias : AudioCodec_OPUS().getAliases()) {
-      codec_map[alias] = opus;
-    }
-
-    auto truehd = []() -> BaseAudioCodec * { return new AudioCodec_TRUEHD(); };
-    for (auto &alias : AudioCodec_TRUEHD().getAliases()) {
-      codec_map[alias] = truehd;
-    }
-
-    auto vorbis = []() -> BaseAudioCodec * { return new AudioCodec_VORBIS(); };
-    for (auto &alias : AudioCodec_VORBIS().getAliases()) {
-      codec_map[alias] = vorbis;
-    }
-  }
-
+  static auto initialize() -> void;
   static std::unordered_map<std::string, std::function<BaseAudioCodec *()>>
       codec_map;
 };
--- src/program/child/ffmpeg/video/VideoCodecFactory.h
+++ src/program/child/ffmpeg/video/VideoCodecFactory.h
@@ -1,119 +1,18 @@
 #ifndef VIDEO_CODEC_FACTORY_H
 #define VIDEO_CODEC_FACTORY_H
 
-#include "../../../settings/enums/Encoders.h"
 #include "BaseVideoCodec.h"
-#include "VideoCodec_AV1.h"
-#include "VideoCodec_AV1_AMF.h"
-#include "VideoCodec_AV1_NVENC.h"
-#include "VideoCodec_AV1_QSV.h"
-#include "VideoCodec_H264.h"
-#include "VideoCodec_H264_AMF.h"
-#include "VideoCodec_H264_NVENC.h"
-#include "VideoCodec_H264_QSV.h"
-#include "VideoCodec_HEVC.h"
-#include "VideoCodec_HEVC_AMF.h"
-#include "VideoCodec_HEVC_NVENC.h"
-#include "VideoCodec_HEVC_QSV.h"
+
 #include <functional>
 #include <string>
 #include <unordered_map>
 
 class VideoCodecFactory {
 public:
-  static auto create(Encoders videoCodec) -> BaseVideoCodec * {
-    switch (videoCodec) {
-    case Encoders::AV1:
-      return new VideoCodec_AV1();
-    case Encoders::AV1_AMF:
-      return new VideoCodec_AV1_AMF();
-    case Encoders::AV1_NVENC:
-      return new VideoCodec_AV1_NVENC();
-      //   case Encoders::AV1_QSV:
-      //     return new VideoCodec_AV1_QSV();
-    case Encoders::H264:
-      return new VideoCodec_H264();
-    case Encoders::H264_AMF:
-      return new VideoCodec_H264_AMF();
-    case Encoders::H264_NVENC:
-      return new VideoCodec_H264_NVENC();
-      //   case Encoders::H264_QSV:
-      //     return new VideoCodec_H264_QSV();
-    case Encoders::HEVC:
-      return new VideoCodec_HEVC();
-    case Encoders::HEVC_AMF:
-      return new VideoCodec_HEVC_AMF();
-    case Encoders::HEVC_NVENC:
-      return new VideoCodec_HEVC_NVENC();
-      //   case Encoders::HEVC_QSV:
-      //     return new VideoCodec_HEVC_QSV();
-    default:
-      return nullptr;
-    }
-  }
-  static auto create(std::string videoCodec) -> BaseVideoCodec * {
-
-    if (codec_map.empty()) {
-      initialize();
-    }
-
-    auto it = codec_map.find(videoCodec);
-    if (it != codec_map.end()) {
-      return it->second();
-    }
-
-    return nullptr;
-  }
+  static auto create(const std::string &name) -> BaseVideoCodec *;
 
 private:
-  auto static initialize() -> void {
-
-    auto av1 = []() { return new VideoCodec_AV1(); };
-    for (auto &alias : VideoCodec_AV1().getAliases()) {
-      codec_map[alias] = av1;
-    }
-
-    auto av1_amf = []() { return new VideoCodec_AV1_AMF(); };
-    for (auto &alias : VideoCodec_AV1_AMF().getAliases()) {
-      codec_map[alias] = av1_amf;
-    }
-
-    auto av1_nvenc = []() { return new VideoCodec_AV1_NVENC(); };
-    for (auto &alias : VideoCodec_AV1_NVENC().getAliases()) {
-      codec_map[alias] = av1_nvenc;
-    }
-
-    auto h264 = []() { return new VideoCodec_H264(); };
-    for (auto &alias : VideoCodec_H264().getAliases()) {
-      codec_map[alias] = h264;
-    }
-
-    auto h264_amf = []() { return new VideoCodec_H264_AMF(); };
-    for (auto &alias : VideoCodec_H264_AMF().getAliases()) {
-      codec_map[alias] = h264_amf;
-    }
-
-    auto h264_nvenc = []() { return new VideoCodec_H264_NVENC(); };
-    for (auto &alias : VideoCodec_H264_NVENC().getAliases()) {
-      codec_map[alias] = h264_nvenc;
-    }
-
-    auto hevc = []() { return new VideoCodec_HEVC(); };
-    for (auto &alias : VideoCodec_HEVC().getAliases()) {
-      codec_map[alias] = hevc;
-    }
-
-    auto hevc_amf = []() { return new VideoCodec_HEVC_AMF(); };
-    for (auto &alias : VideoCodec_HEVC_AMF().getAliases()) {
-      codec_map[alias] = hevc_amf;
-    }
-
-    auto hevc_nvenc = []() { return new VideoCodec_HEVC_NVENC(); };
-    for (auto &alias : VideoCodec_HEVC_NVENC().getAliases()) {
-      codec_map[alias] = hevc_nvenc;
-    }
-  }
-
+  static auto initialize() -> void;
   static std::unordered_map<std::string, std::function<BaseVideoCodec *()>>
       codec_map;
 };
