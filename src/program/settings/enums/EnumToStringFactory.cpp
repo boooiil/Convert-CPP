73e09563d2b2f2d709e61a14a1ec0ed3f9d8956b
boooiil <boooiil@outlook.com>
2025-11-13 00:14:44 -0500

    rm in favor of Definitions

--- src/program/settings/enums/EnumToStringFactory.cpp
+++
@@ -1,5 +0,0 @@
-// EnumToStringFactory.cpp
-#include "EnumToStringFactory.h"
-#include "src/program/registry/EnumToStringRegistryMaster.h"
-
-EnumToStringRegistryMaster EnumToStringFactory::registries;
