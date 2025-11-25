8216a4702551b8ffc883d33b0ea5721b6f0ec542
boooiil <boooiil@outlook.com>
2025-11-24 22:52:20 -0500

    add sub mapping

--- src/program/child/ffmpeg/subtitle/BaseSubtitleCodec.h
+++ src/program/child/ffmpeg/subtitle/BaseSubtitleCodec.h
@@ -12,6 +12,17 @@ public:
     return "Base Subtitle Codec";
   };
   virtual auto getAliases() -> std::set<std::string> { return {}; };
+  virtual auto isImage() -> bool { return false; };
+
+  void setIndex(int idx) { this->index = idx; }
+  int getIndex() { return this->index; }
+
+  void setMapIndex(int idx) { this->mapIndex = idx; }
+  int getMapIndex() { return this->mapIndex; }
+
+private:
+  int index;
+  int mapIndex;
 };
 
 #endif // BASE_SUBTITLE_CODEC_H
