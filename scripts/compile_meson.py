2c751db0f41af0371378e4e506bc453c5187248f
boooiil <boooiil@outlook.com>
2026-03-20 07:40:08 -0400

    use Path to handle paths, fix missing parenthesis on is_dir

--- scripts/compile_meson.py
+++ scripts/compile_meson.py
@@ -1,25 +1,24 @@
-import os
 import sys
 from pathlib import Path
 
-def readDirRecursive(directory):
+def readDirRecursive(directory: Path):
 
     # exit if invalid path
-    if not Path(directory).exists():
+    if not directory.exists():
         print(f"Directory '{directory}' does not exist.")
-        exit()
+        sys.exit()
 
     root = directory
-    built = f"# root/{root}meson.build\n\n"
+    built = f"# root/{root.as_posix()}/meson.build\n\n"
 
-    for file in Path(directory).glob("*"):
+    for file in Path(root).glob("*"):
         
-        path = root + file
+        path: Path = root / file.name
         
         # if dir, recursive
-        if file.is_dir:
+        if file.is_dir():
             built += f"subdir('{file.name}')\n"
-            readDirRecursive(path + '/')
+            readDirRecursive(path)
             continue
 
         # ignore non cpp files
@@ -32,7 +31,7 @@ def readDirRecursive(directory):
         else:
             built += f"src_files += files('{file.name}')\n"
 
-    build_file = Path(root + "meson.build")
+    build_file = Path(root / "meson.build")
 
     if build_file.exists() and build_file.is_file():
         build_file_content = build_file.read_text()
@@ -40,10 +39,16 @@ def readDirRecursive(directory):
             build_file.write_text(built)
             print(f"meson.build... Updated.")
         else:
-            print(f"meson.build... Skipped (up to date).")
+            # print(f"meson.build... Skipped (up to date).")
+            pass
     else:
         build_file.write_text(built)
         print(f"meson.build... Created.")
 
-readDirRecursive(sys.argv[1])
+
+if (len(sys.argv) != 2):
+    print("Usage: python compile_meson.py <directory>")
+    sys.exit()
+
+readDirRecursive(Path(sys.argv[1]))
 print("Meson OK.")
